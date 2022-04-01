853635d7d53b0fde09d4a95147e9a981820284bc
hana78786 <102535815+hana78786@users.noreply.github.com>
2022-04-01 23:26:36 +0900

    Delete src directory

--- src/bin/hello_world.java
+++
@@ -1,9 +0,0 @@
-package bin;
-
-public class hello_world {
-	public static void main(String[] args) {
-		System.out.println("hello world123");
-		System.out.println("java test!!!");
-	}
-
-}
--- src/test_world.java
+++
@@ -1,9 +0,0 @@
-package bin;
-
-public class test_world {
-	public static void main(String[] args) {
-		System.out.println("test_world") ;
-		System.out.println("재 테스트211");
-	}
-
-}
