7858e807db82b65bc4309a4f68083f83fe5245b0
hana78786 <upup78786@gmail.com>
2022-04-01 22:01:31 +0900

    자바 테스트 추가

--- src/bin/hello_world.java
+++ src/bin/hello_world.java
@@ -3,7 +3,7 @@ package bin;
 public class hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("jana test");
+		System.out.println("java test!");
 	}
 
 }
