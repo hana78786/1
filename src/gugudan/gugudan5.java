88d80f094b7bde2844093c3f91d8f1965f5cfc33
hana78786 <upup78786@gmail.com>
2022-04-02 22:35:02 +0900

    67단 구현

---
+++ src/gugudan/gugudan5.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gugudan;
+public class gugudan5 {
+	public static void main(String[] args) {
+		System.out.println("6단");
+		int i=1;
+		while(i<10){ 
+			System.out.println(6*i); //6Xi의 값을 출력한다
+			i=i+1; //i의 값을 i+1으로 다시 변경한다
+
+	}
+		System.out.println("7단");
+		for(int c = 1;c<10;c++) {
+			System.out.println(7*c);
+		}
+	} 
+
+}
