fcf261454adf171c1a1ea0f032dd88090fcc7040
hana78786 <upup78786@gmail.com>
2022-04-03 21:22:07 +0900

    2중 for문을 이용한 구구단의 반복 표현

---
+++ src/a/gugudan5.java
@@ -0,0 +1,14 @@
+package a;
+public class gugudan5 {
+    public static void main(String[] args) {
+        int[] result = new int[9];
+        for(int j=2;j<10;j++) {
+        for(int i=0;i<result.length; i++) {
+        	 {
+        		result[i]=j*(i+1);
+        	System.out.println(result[i]);
+        	}}}}} 
+        	
+           
+      
+    
