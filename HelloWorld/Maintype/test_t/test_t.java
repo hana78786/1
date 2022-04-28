e17c68ce2b18a50012dbe7f9c6531c2eae62b36a
hana78786 <upup78786@gmail.com>
2022-04-29 01:07:01 +0900

    commit test2

---
+++ HelloWorld/Maintype/test_t/test_t.java
@@ -0,0 +1,14 @@
+package test_t;
+
+public class test_t {
+
+	public test_t() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("1234");
+
+	}
+
+}
