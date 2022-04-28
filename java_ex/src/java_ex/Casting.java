68c8c78e9bc3d7a71c129a8dd367234d7925abd1
hana78786 <upup78786@gmail.com>
2022-04-29 01:41:03 +0900

    casting의 활용

---
+++ java_ex/src/java_ex/Casting.java
@@ -0,0 +1,21 @@
+package java_ex;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		double b = 1;
+		System.out.println(b);
+		
+		int c= (int) 1.1;
+		System.out.println(c);
+		
+
+		String number = Integer.toString(10);
+
+		System.out.println(number.getClass());
+		
+		
+	}
+
+}
