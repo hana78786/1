980bafa9c0118502d433bd38d18604e64af2fb82
hana78786 <upup78786@gmail.com>
2022-04-29 01:30:14 +0900

    함수의 정의

---
+++ java_ex/src/java_ex/valuable.java
@@ -0,0 +1,21 @@
+package java_ex;
+
+public class valuable {
+
+	public valuable() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int a = 1; //정수
+		System.out.println(a);
+		
+		double b = 1.1; // 실수
+		System.out.println(b);
+		
+		String c="Hello world"; //문자열
+		System.out.println(c);
+
+	}
+
+}
