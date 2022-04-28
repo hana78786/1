95c217a36e1bda8b00126dee841e596ac0a48f3a
hana78786 <upup78786@gmail.com>
2022-04-29 01:23:21 +0900

    이클립스 오류로 자바폴더 재생성

---
+++ java_ex/src/java_ex/test.java
@@ -0,0 +1,14 @@
+package java_ex;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
