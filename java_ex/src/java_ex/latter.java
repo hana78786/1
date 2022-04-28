fb34a5b571ea72b52b935d647e13795680794057
hana78786 <upup78786@gmail.com>
2022-04-29 01:33:00 +0900

    변수의 사용

---
+++ java_ex/src/java_ex/latter.java
@@ -0,0 +1,18 @@
+package java_ex;
+
+public class latter {
+
+	public latter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "hana";
+		System.out.println("Hello "+name+"..."+name);
+		
+		double Vat=10.0;
+		System.out.println(Vat);
+	}
+
+}
