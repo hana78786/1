5309bfc268fe2cba43ce65681346a1d7dea5c73f
hana78786 <upup78786@gmail.com>
2022-05-01 22:09:30 +0900

    class사용하기

---
+++ java_ex/src/java_ex/ClassApp.java
@@ -0,0 +1,13 @@
+package java_ex;
+
+public class ClassApp {
+
+public static void main(String[] args) {
+	
+	System.out.println(Math.PI);
+	System.out.println(Math.floor(Math.PI));
+	System.out.println(Math.ceil(Math.PI));
+
+	
+	
+}}
