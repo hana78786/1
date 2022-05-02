d283ba67eff74c0697938de695adf87b996b994f
hana78786 <upup78786@gmail.com>
2022-05-03 01:30:37 +0900

    instance

--- java_ex/src/myapp/MyClassApp.java
+++ java_ex/src/myapp/MyClassApp.java
@@ -1,9 +1,9 @@
 package myapp;
 
 class Acounting{
-public static double valueOfSupply;
-public static double vatRate;
-public static double espeseRate;
+public  double valueOfSupply;
+public  double vatRate;
+public  double espeseRate;
 
 double vat = getVAT();
 double totla = getTotal();
@@ -13,7 +13,7 @@ double dividend1 = getDividend1();
 double divideend2 = getDividend2();
 double dividend3 = getDividend3();
 
-static void print() {
+void print() {
 	System.out.println("Value of supply : "+valueOfSupply);
 	System.out.println("VAT : "+getVAT());
 	System.out.println("Totla : "+getTotal());
@@ -24,31 +24,31 @@ static void print() {
 	System.out.println("Dividend 3 : "+getDividend3());
 }
 
-private static double getDividend3() {
+private double getDividend3() {
 	return getIncome()*0.2;
 }
 
-private static double getDividend2() {
+private double getDividend2() {
 	return getIncome()*0.3;
 }
 
-private static double getDividend1() {
+private double getDividend1() {
 	return getIncome()*0.5;
 }
 
-private static double getIncome() {
+private double getIncome() {
 	return valueOfSupply -getExpense();
 }
 
-private static double getExpense() {
+private double getExpense() {
 	return valueOfSupply* espeseRate;
 }
 
-private static double getTotal() {
+private double getTotal() {
 	return valueOfSupply+getVAT();
 }
 
-private static double getVAT() {
+private double getVAT() {
 	return valueOfSupply*vatRate;
 }
 }
@@ -57,12 +57,18 @@ private static double getVAT() {
 public class MyClassApp {
 
 public static void main(String[] args) {
-	
-	
-	Acounting.valueOfSupply= 10000;
-	Acounting.vatRate = 0.1;
-	Acounting.espeseRate = 0.3;
 
+	Acounting a1=new Acounting();
+	a1.valueOfSupply= 10000;
+	a1.vatRate = 0.1;
+	a1.espeseRate = 0.3;
+	a1.print();
+	System.out.println();
 	
-	Acounting.print();
+	Acounting a2=new Acounting();
+	a2.valueOfSupply= 20000;
+	a2.vatRate = 0.2;
+	a2.espeseRate = 0.5;
+	a2.print();
+			
 }}
