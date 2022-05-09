4ffb00105c1ccd61f293cae0f4dc686c73624877
hana78786 <upup78786@gmail.com>
2022-05-10 00:13:51 +0900

    배열합계

--- cording_test/src/co_te/pumi.java
+++ cording_test/src/co_te/pumi.java
@@ -3,33 +3,25 @@ package co_te;
 import java.util.Scanner;
 
 public class pumi {
+int[][] arr1= new int[][] {};
+	int[][] arr2=new int[][] {};
+	int[][] arr=add(arr1,arr2);
+	
+	public int[][] solution(int[][] arr1, int[][] arr2) {
 
-	public pumi() {
-		//행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
-//		제한 조건
-//
-//	    행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
-//
-//	입출력 예
-//	arr1 	arr2 	return
-//	[[1,2],[2,3]] 	[[3,4],[5,6]] 	[[4,6],[7,9]]
-//	[[1],[2]] 	[[3],[4]] 	[[4],[6]]
+		int[][] answer = {};
+	        return answer;
+	    }
+	
+	private int[][] add(int[][] arr12, int[][] arr22) {
+		int length=arr12.length<arr22.length?arr12.length
+				: arr2.length;
+		return null;
 	}
 
 	public static void main(String[] args) { 
-	
-		int[][] arr1= {	{1,2},{3,4}
-		  };
-		  int[][] arr2= {{3,4},{5,6}
-		  };
-		  int[][]result=new int[2][2];
-		  
-		  for (int i=0;i<result.length;i++) {
-			  int sum = 0;
-			  for(int j=0;j<result.length;j++) {
-				  arr1[i][result.length]+=arr2[i][result.length];
-			  }
-			  arr1[i][result.length]=sum;
-		  };
-	
+		 
+		 
+		
+//	System.out.println(sum);
 }}
