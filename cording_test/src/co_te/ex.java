c760e564dd1e80e7866ce0acb9d3782d2365cc3d
hana78786 <upup78786@gmail.com>
2022-05-10 00:13:56 +0900

    연습

--- cording_test/src/co_te/ex.java
+++ cording_test/src/co_te/ex.java
@@ -2,15 +2,23 @@ package co_te;
 
 public class ex {
 
-	public ex() {
-		// TODO Auto-generated constructor stub
+	int[][] arr1= new int[][] {{1,2},{3,4}};
+	int[][] arr2=new int[][] {{5,6},{7,8}};
+	int[][] arr=add(arr1,arr2);
+	
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+
+		int[][] answer = {};
+	        return answer;
+	    }
+	
+	private int[][] add(int[][] arr12, int[][] arr22) {
+		int length=arr12.length<arr22.length?arr12.length
+				: arr2.length;
+		
+		for(int i=0;i<length;i++) {
+			length[i]= arr1[i] + arr2[i];
+		}
+		return null;
 	}
-	public static void main(String[] args) {
-		System.out.println(d(3,"*"));
-	}
-	  public static String d(int c, String d) {   
-		   for(int j=0;j<c;j++) {
-		     d+="*";
-		      }
-			return d;  }
 }
