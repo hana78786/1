1fa50fde31c783f30e7615d44970de297d44288d
hana78786 <upup78786@gmail.com>
2022-05-10 00:13:45 +0900

    하샤드 수

---
+++ cording_test/src/co_te/hashad.java
@@ -0,0 +1,27 @@
+package co_te;
+
+public class hashad {
+
+	
+	public static String x="10000";
+	public static int a=x.length();
+	
+	
+	public static <retrun> void y(String x, int a) {
+		for(int i=0;i<a;i++) {
+			String y="";
+			y=x.substring(a-i-1);	
+			System.out.println(y);
+		};
+		retrun y;
+
+	}
+public static void main(String[] args) {
+y(x,a);
+	
+}
+	public boolean solution(int x) {
+        boolean answer = true;
+        return answer;
+    }
+}
