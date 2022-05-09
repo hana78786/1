7706f944951722d9587ae7df832dcff18926e74c
hana78786 <upup78786@gmail.com>
2022-05-10 01:21:19 +0900

    문제를 잘못이해해서 다시 작성...완료

--- cording_test/src/co_te/hashad.java
+++ cording_test/src/co_te/hashad.java
@@ -2,26 +2,25 @@ package co_te;
 
 public class hashad {
 
-	
-	public static String x="10000";
-	public static int a=x.length();
-	
-	
-	public static <retrun> void y(String x, int a) {
-		for(int i=0;i<a;i++) {
-			String y="";
-			y=x.substring(a-i-1);	
-			System.out.println(y);
-		};
-		retrun y;
 
-	}
-public static void main(String[] args) {
-y(x,a);
 	
-}
-	public boolean solution(int x) {
-        boolean answer = true;
+    public boolean solution(int x) {
+    	String str_x=Integer.toString(x);
+        int a=str_x.length();
+     String[] y = new String[a];
+		for(int i=0;i<a;i++) {	
+		y[i]=str_x.substring(i,i+1);
+		};
+		int[] str_y=new int[a];
+		for (int d=0;d<a;d++) {
+		str_y[d]=Integer.parseInt(y[d]);
+		};
+
+		int sum=0;
+		
+		boolean answer;
+		if(x%sum==0) {answer=true;}
+		else { answer=false;}
         return answer;
     }
 }
