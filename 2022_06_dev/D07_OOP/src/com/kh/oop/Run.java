48173eb0513589a1000e9150faf37a664bb191b5
unknown <upup78786@gmail.com>
2022-06-22 18:00:26 +0900

    객체지향

---
+++ 2022_06_dev/D07_OOP/src/com/kh/oop/Run.java
@@ -0,0 +1,20 @@
+package com.kh.oop;
+
+public class Run {
+	public static void main(String[] args) {
+//			Person.hobby="잠자기";
+//			Person person = new Person();
+//			person.setAddress ("서울시 종로구");
+//			System.out.println(person.getAddress());
+//			person.setName("일용자");
+//			System.out.println(person.getName());
+//			person.gender='남';
+//			
+//			person.takeWalk(1);
+//			person.dialPhone(123456);
+		
+		Suit suit = new Suit(100);
+		
+	}
+
+}
