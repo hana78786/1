f3108722810838f74cf62362d94ae30f0a94221b
hana78786 <upup78786@gmail.com>
2022-06-29 00:51:44 +0900

    입출력 연습

---
+++ 2022_06_dev/Exercise/src/Ex1/FileExist.java
@@ -0,0 +1,31 @@
+package Ex1;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileExist {
+public static void main(String[] args) {
+	File file = new File("D:/temp/file.txt");
+	
+	
+	if(file.exists() == false) {file.mkdirs();}
+	boolean isExistif = file.exists();
+	System.out.println(isExistif);
+	
+	File temp = new File("D:/Temp");
+	File[] contents = temp.listFiles();
+	System.out.println("날짜                 시간        형태         크기       이름");
+	System.out.println("------------------------------------");
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
+	for(File file1 : contents) {
+		System.out.println(sdf.format(new Date(file.lastModified())));
+		if(file.isDirectory()) {
+			System.out.println("\t<file1>\t\t\t" + file.getName());
+		}
+		else {
+			System.out.println("\t\t\t + fil1.lenght + "\t" +file1.get);
+		}
+	}
+}
+}
---
+++ cording_test/src/Ex/BufferedReaderEx.java
@@ -0,0 +1,23 @@
+package Ex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+public static void main(String[] args) {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("입력하세요");
+	try {
+		String str = br.readLine();
+		System.out.println(str);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+	public BufferedReaderEx() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
