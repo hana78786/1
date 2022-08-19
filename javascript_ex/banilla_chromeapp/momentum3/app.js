c6c264762f03e7f31c9c0c848bb6f071406e6037
hana78786 <upup78786@gmail.com>
2022-08-19 23:07:24 +0900

    노마드코더 강의 따라하기

--- javascript_ex/banilla_chromeapp/momentum3/app.js
+++ javascript_ex/banilla_chromeapp/momentum3/app.js
@@ -4,3 +4,23 @@ console.log(title);
 
 const title1=document.querySelectorAll(".hello h1")
 console.log(title1);
+
+
+
+function clickevent(){
+title1[1].style.color="blue"
+}
+
+title1[1].addEventListener("click",clickevent);
+
+console.dir(title);
+
+function moseEnt(){
+    title.innerHTML="mouse is here"
+}
+function moseLeave(){
+    title.innerHTML="mouse is out"
+}
+
+title.addEventListener("mouseenter",moseEnt)
+title.addEventListener("mouseleave",moseLeave)
