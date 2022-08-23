823c6707d816177ef4d4b8b5aca666000956f7ef
hana78786 <upup78786@gmail.com>
2022-08-24 00:02:47 +0900

    노마드코딩 연습

--- javascript_ex/banilla_chromeapp/momentum3/app.js
+++ javascript_ex/banilla_chromeapp/momentum3/app.js
@@ -2,10 +2,10 @@ const h1 = document.querySelector("div.hello:first-child h1")
 
 const Cname ="clicked";
 function hadnleTitleClick(){
-   if(h1.className==="active"){
-    h1.className=Cname;
+   if(h1.classList.contains("active")){
+    h1.classList.remove("active");
    }else{
-   h1.className="active";}
+   h1.classList.add("active");}
    console.log(h1.className)
 
 }
