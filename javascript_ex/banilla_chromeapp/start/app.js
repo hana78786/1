56a35d4a957c75ff403ea6bf5cbf808f5845c356
hana78786 <upup78786@gmail.com>
2022-08-28 19:34:27 +0900

    로그인한 아이디값 기억하기

--- javascript_ex/banilla_chromeapp/start/app.js
+++ javascript_ex/banilla_chromeapp/start/app.js
@@ -6,16 +6,31 @@ const loginForm = document.querySelector("#login-form");
 const link = document.querySelector('a');
 const greetning = document.querySelector('#greeting')
 const HIDDEN_CLASSNAME="hidden" //문자열을 담은 상수는 대문자로 표기한다
+const USERNAME_KEY="username;"
+const saveUserName = localStorage.getItem(USERNAME_KEY)
 
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    // greetning.innerText="Hello "+username;
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreeting(username)
+
+}
+
+function paintGreeting(username){
     greetning.innerText=`Hello ${username}`;
     greetning.classList.remove(HIDDEN_CLASSNAME);
 
 }
 
-loginForm.addEventListener("submit",onLoginSubmit)
+
+if(saveUserName==null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit)
+    
+}else{
+   paintGreeting(saveUserName)
+
+}
