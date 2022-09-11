e7a68bda373e07946e635cecd0124edcf9610ab9
hana78786 <upup78786@gmail.com>
2022-09-11 23:12:58 +0900

    todoList 삭제하기 노마드코더

--- javascript_ex/banilla_chromeapp/start1/js/todo.js
+++ javascript_ex/banilla_chromeapp/start1/js/todo.js
@@ -2,20 +2,38 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const todoInput = toDoForm.querySelector("input");
 
+
 function paintToDo(newTodo){
     const li = document.createElement('li'); //li태그 생성
     const span = document.createElement('span'); //span태그 생성
-    li.appendChild(span); //<li><span></spam></li>
     span.innerText=newTodo;
+    const button = document.createElement('button');
+    button.className='delete';
+    button.innerText='❌';
+    
+    
+   
+   button.addEventListener("click", deleteToDo) //클릭이벤트 생성
+        
+   
+    
+   
+    li.appendChild(span); //<li><span></spam></li>
+    li.appendChild(button);
     toDoList.appendChild(li);
-    console.log(newTodo)
+
+ 
+}
+
+function deleteToDo(even){
+    const li = event.target.parentElement//클릭한 현재 이벤트의 타겟의 부모태그속성
+    li.remove();
 }
 
 
 function handlToDoSubmit(event){
     event.preventDefault();
-    console.log("?")
-    const newTodo=todoInput.value;
+        const newTodo=todoInput.value;
     todoInput.value="";
     paintToDo(newTodo)
 
