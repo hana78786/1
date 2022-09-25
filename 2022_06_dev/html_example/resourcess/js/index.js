ca3df22f2cf0ec302e0d16878562749d1fcb2d75
hana78786 <upup78786@gmail.com>
2022-09-25 22:25:37 +0900

    과제용 html 작성중

---
+++ 2022_06_dev/html_example/resourcess/js/index.js
@@ -0,0 +1,72 @@
+window.onload=function(){
+var down=document.querySelector('#down-icon');
+var sec=document.querySelector('#secret-area');
+var up=document.querySelector('#up-icon');
+down.onclick=ViewRecipe;
+up.onclick=removeRecipe;
+
+
+function ViewRecipe(){
+    sec.style.display='flex';
+    down.style.display='none';
+    up.style.display='';
+    down.style.transition='0.5s';
+    up.style.transition='0.5s';
+    
+
+}
+function removeRecipe(){
+    sec.style.display='none';
+    down.style.display='';
+    up.style.display='none';
+    down.style.transition='0.5s';
+    up.style.transition='0.5s';
+
+}
+
+
+window.addEventListener('scroll',function(){
+
+    if(window.scrollY>1300){
+        document.querySelector('.text').style.animation='slide-no 2s ease-out forwards';
+
+
+    }
+    
+    if(window.scrollY>300&&window.scrollY<1200){
+        document.querySelector('.text').style.animation='slide 2s ease-out forwards';
+
+
+    }
+
+    if(window.scrollY>1300){
+        document.querySelector('.text1').style.animation='slide-no 2s ease-out forwards';
+
+
+    }
+    
+    if(window.scrollY>300&&window.scrollY<1200){
+        document.querySelector('.text1').style.animation='slide1 2s ease-out forwards';
+
+
+    }
+
+    if(window.scrollY>300&&window.scrollY<1200){
+        document.querySelector('#pola-img').style.animation='fadein 3s ease-out forwards';
+
+
+    }
+
+    if(window.scrollY>1300){
+        document.querySelector('#pola-img').style.animation='fadeout 3s ease-out forwards';
+
+
+    }
+});
+
+
+
+
+
+
+}
