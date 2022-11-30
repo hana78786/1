af059ef8e3115bdd45907717cf74d322908dcf56
hana78786 <upup78786@gmail.com>
2022-11-30 23:27:06 +0900

    현재 위치 좌표로 만들기

---
+++ javascript_ex/banilla_chromeapp/start1/js/weather.js
@@ -0,0 +1,12 @@
+function onGeoOk(position){
+    
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("you live in",lat, lon )
+}
+
+function onGeoError(){
+    alert("can't find you. No weather for you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError) //현재 와이파이 gps 등의 정보를 넘겨줌 ()안에 성공, 실패 함수를 넣어주어 실행한다.
