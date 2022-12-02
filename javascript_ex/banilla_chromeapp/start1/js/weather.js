07b615f7ecfc73a6b72756fbc79c85b4db2fd825
hana78786 <upup78786@gmail.com>
2022-12-03 00:16:55 +0900

    콘솔로 현재 날씨 출력하기

--- javascript_ex/banilla_chromeapp/start1/js/weather.js
+++ javascript_ex/banilla_chromeapp/start1/js/weather.js
@@ -5,8 +5,10 @@ function onGeoOk(position){
     
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=+${lat}&lon=${lon}&appid=${apiKey}`;
-    fetch(url)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metri`;
+    fetch(url).then(response => response.json().then(data =>{
+        console.log(data.weather[0].main)
+    } ));
 }
 
 function onGeoError(){
