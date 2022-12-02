f749b1e381b9fa35f6a8c68003794bfd4f155e60
hana78786 <upup78786@gmail.com>
2022-12-02 23:26:15 +0900

    api를 사용하여 날씨 url접속하기

--- javascript_ex/banilla_chromeapp/start1/js/weather.js
+++ javascript_ex/banilla_chromeapp/start1/js/weather.js
@@ -5,7 +5,8 @@ function onGeoOk(position){
     
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("you live in",lat, lon )
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=+${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(url)
 }
 
 function onGeoError(){
