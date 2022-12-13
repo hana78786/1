5141844eca113f7cc774b5aca7b239c711ac06c1
hana78786 <upup78786@gmail.com>
2022-12-14 00:05:54 +0900

    날씨 입력완성

--- javascript_ex/banilla_chromeapp/start1/js/weather.js
+++ javascript_ex/banilla_chromeapp/start1/js/weather.js
@@ -8,9 +8,14 @@ function onGeoOk(position){
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     fetch(url)
         .then((response) => response.json())
-        .then((data) =>{
-        const name = data.name, data.weather[0].main);
-         });
+        .then((data) => {
+        const weatherContainer = document.querySelector('#weather span:first-child');
+        const cityContainer = document.querySelector('#weather span:last-child');
+        const name = data.name;
+        const weather =  data.weather[0].main;
+        weatherContainer.innerHTML= weather;
+       cityContainer.innerHTML=name;
+        });
 }
 
 function onGeoError(){
