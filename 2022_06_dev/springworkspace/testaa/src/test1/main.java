4f13556c25a9330804f65705187c4b1342e4a928
hana78786 <upup78786@gmail.com>
2022-12-26 22:22:34 +0900

    2021226

---
+++ 2022_06_dev/springworkspace/testaa/src/test1/main.java
@@ -0,0 +1,16 @@
+package test1;
+
+import java.util.Scanner;
+
+public class main {
+	
+	public static void Main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		a =  sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+	}
+
+}
