03bbb11cee9e123e415c3e686657dad0c18736dd
hana78786 <upup78786@gmail.com>
2023-01-18 00:19:45 +0900

    0117-1

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0117.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0117.java
@@ -1,8 +1,27 @@
 package test1;
 
-public class Test0117 {	
+import java.util.Scanner;
+
+public class Test0117 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int count = 0;
+		int n = 9999;
+		int a = 9999;
+		while (a != num) {
+			if (count == 0) {
+				n = num / 10 + num % 10;
+				a = (num % 10) * 10 + n%10;
+			}else {
+				n = a/10 + a%10;
+				a = (a%10)*10+n%10;
+			}
+
+			count++;
+
+		}
+		System.out.println(count);
 	}
 
 }
