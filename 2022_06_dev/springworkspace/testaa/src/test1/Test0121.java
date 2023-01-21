3ef91b3026ba5b362bbfa62754b0b49cabe3a301
hana78786 <upup78786@gmail.com>
2023-01-21 23:00:48 +0900

    0121

---
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0121.java
@@ -0,0 +1,21 @@
+package test1;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Test0121 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int[] b = new int[a];
+		for(int i=0;i<a;i++) {
+			b[i] = sc.nextInt();
+		}
+		Arrays.sort(b);
+		System.out.println(b[0]+" "+b[a-1]);
+		
+		
+	}
+
+}
