c354783b581d0fb1e469104422b901094964c126
hana78786 <upup78786@gmail.com>
2023-01-25 23:28:40 +0900

    0125

---
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
@@ -0,0 +1,20 @@
+package test1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Test0125 {
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		Scanner sc = new Scanner(System.in);
+		HashSet<Integer> list =new HashSet<Integer>(); 
+		for (int i = 0; i < 10; i++) {
+			list.add(sc.nextInt()%42);	
+
+		}
+		System.out.println(list.size());
+		
+		
+	}
+
+}
