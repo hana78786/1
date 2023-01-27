44ad83cb16017a993d0e04c6aea98929122a2472
hana78786 <upup78786@gmail.com>
2023-01-27 23:12:53 +0900

    0127

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
@@ -1,19 +1,47 @@
 package test1;
 
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class Test0125 {
 	public static void main(String[] args) {
-		int[] arr = new int[10];
 		Scanner sc = new Scanner(System.in);
-		HashSet<Integer> list =new HashSet<Integer>(); 
-		for (int i = 0; i < 10; i++) {
-			list.add(sc.nextInt()%42);	
+		double[] arr = new double[sc.nextInt()];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
 
 		}
-		System.out.println(list.size());
-		
+		double max = arr[0];
+		double min = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			int mincount = 0;
+
+			if (mincount == 0 && min == arr[i]) {
+				double a = (min/max)*100;
+				mincount++;
+			}
+
+		}
+		int sum =0;
+		int avg=0;
+		for(int i =0;i<arr.length;i++) {
+			sum += arr[i];
+		}
+		avg = sum/arr.length;
+		System.out.println(avg);
 		
 	}
 
