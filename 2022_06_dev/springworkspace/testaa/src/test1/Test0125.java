662eafc64414ed1824144918d3f740c90b363a09
hana78786 <upup78786@gmail.com>
2023-01-31 23:01:07 +0900

    0131

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
@@ -12,7 +12,6 @@ public class Test0125 {
 
 		}
 		double max = arr[0];
-		double min = arr[0];
 
 		for (int i = 0; i < arr.length; i++) {
 
@@ -20,30 +19,22 @@ public class Test0125 {
 				max = arr[i];
 			}
 
-			if (min > arr[i]) {
-				min = arr[i];
-			}
-
 		}
 
 		for (int i = 0; i < arr.length; i++) {
-			int mincount = 0;
-
-			if (mincount == 0 && min == arr[i]) {
-				arr[i] = Math.round((min/max)*10000);
-				arr[i]= arr[i]/100;
-				mincount++;
+	
+				arr[i] = (arr[i]/max)*100;
+				
+				
 			}
 
-		}
+		
 		double sum =0;
 		double avg=0;
 		for(int i =0;i<arr.length;i++) {
 			sum += arr[i];
 		}
 		avg = sum/arr.length;
-		avg = Math.round(avg*100);
-		avg = avg/100;
 		System.out.println(avg);
 		
 	}
