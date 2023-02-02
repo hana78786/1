8b0c243e34152f5ad6682e7886a06405543ee597
hana78786 <upup78786@gmail.com>
2023-02-02 23:49:17 +0900

    0202

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0125.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class Test0125 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	String str = sc.next();
+	String str = "";
+	int score = 0;
+	
+	String[] arrStr = str.split("");
+	for(int i=0; i<sc.nextInt();i++) {
+		str = sc.next();
+	}
+	for(int i =0; i<arrStr.length;i++) {
+		if(i==0) {
+			if(arrStr[i] == "O");
+			score = 1;
+		}
+	}
 	System.out.println(str);
 	
 	
