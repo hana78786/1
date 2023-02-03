86ec62388073bd90d9eb7cc890dc1b5f6613284e
hana78786 <upup78786@gmail.com>
2023-02-04 00:02:09 +0900

    0203

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0203.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0203.java
@@ -7,6 +7,16 @@ public class Test0203 {
 		Scanner sc = new Scanner(System.in);
 		for(int i=0;i<sc.nextInt();i++) {
 			String str = sc.next();
+			String[] strArr = str.split("");
+			int num = 0;
+			int count = 0;
+			for(int j=0;j<strArr.length;j++) {
+				if(strArr[j].equals("O")) {
+					count++;
+					num=num+count;
+				}
+				System.out.println(num);
+			}
 		}
 		
 	}
