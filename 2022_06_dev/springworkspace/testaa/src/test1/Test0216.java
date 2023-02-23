297b59b984adf51837eaebc9e3749617f528b154
hana78786 <upup78786@gmail.com>
2023-02-23 22:34:15 +0900

    0223

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0216.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0216.java
@@ -6,13 +6,12 @@ public class Test0216 {
 	public static void main(String[] args) {
 
 		int n = 0;
-		String b = String.valueOf(a);
+		String b = String.valueOf(1000);
 		while(n<1000) {
 			int[] c = new int[b.length()];
 			for(int i=0;i<c.length;i++) {
 				c[i]=Integer.parseInt(b.substring(i, i+1));
 			}
-			n=n+a;
 			for(int i=0; i<c.length; i++) {
 				n+=c[i];
 				System.out.println(n);
