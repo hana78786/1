6237fd1f82b99c2ceba8168275da4d8818d61c40
hana78786 <upup78786@gmail.com>
2023-03-17 23:53:19 +0900

    0317

--- 2022_06_dev/springworkspace/testaa/src/test1/Test316.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test316.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Test316 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		long num = sc.nextLong();
+		long num = sc.nextInt();
 		long a = num/4;
 		String b ="";
 		for(int i=0; i<a;i++) {
 			b+="long ";
 		}
-		System.out.println(b+" int");
+		System.out.println(b+"int");
 		
 	}
 
