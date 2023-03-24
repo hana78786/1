ba86a6060f158102d3a6b5d9e8ee4b42df4057e2
hana78786 <upup78786@gmail.com>
2023-03-24 21:26:55 +0900

    0324

--- 2022_06_dev/springworkspace/testaa/src/test1/Test0323.java
+++ 2022_06_dev/springworkspace/testaa/src/test1/Test0323.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Test0323 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		for (int i=0;i<m;i++) {
 
-		
+		}
 	}
 
 }
